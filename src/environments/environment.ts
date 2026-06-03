841e03bcb6bf11c65faf8a355739b5507dc0d912
F@ij <76701796+mohdfaij007@users.noreply.github.com>
2026-06-03 10:36:36 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
  // apiUrl: 'http://localhost:8080'
-apiUrl: 'schoolmanagementrailway-production-e2e6.up.railway.app'
+apiUrl: 'https://schoolmanagementrailway-production-e2e6.up.railway.app'
 };
